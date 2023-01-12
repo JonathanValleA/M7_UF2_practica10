1569ca00eb23987d5625f5e2c73bfb3e4b06b5ae
JonathanValleA <jvalle@jaumebalmes.net>
2023-01-12 20:47:08 +0100

    Agregar archivo ejercicio3 y el main.py

---
+++ ejercicio3.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+def velocidadMicro():
+    df = pd.read_csv("test.csv", usecols=["clock_speed"])
+    print(df)
+
+def megapixels():
+    df = pd.read_csv("")
