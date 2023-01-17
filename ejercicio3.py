1231b75967304579ec6e1a547db8e58b510e6387
JonathanValleA <113449282+JonathanValleA@users.noreply.github.com>
2023-01-17 20:54:02 +0100

    Implementar grafica en las 3 funciones creadas en el ejercicio3.py (#2)
    * Implementar grafica en las 3 funciones creadas en el ejercicio3.py
    
    * Agregar comentarios

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,8 +1,34 @@
+# Importamos pandas
 import pandas as pd
-import matplotlib.pyplot as plt
-def velocidadMicro():
-    df = pd.read_csv("test.csv", usecols=["clock_speed"])
-    print(df)
 
-def megapixels():
-    df = pd.read_csv("")
+### Funcion Principal para la lectura de archivo csv y filtrar los datos por el id ###
+def leer_y_filtrar_datos(id_a_filtrar):
+    df = pd.read_csv("test.csv")
+    id = df[df['id'].isin(id_a_filtrar)]
+    return id
+
+# 1r Funcion para mostrar los datos de clock_speed por los id que le pasemos
+def mostrar_clock_speed(id_a_filtrar):
+    data = leer_y_filtrar_datos(id_a_filtrar)
+    """ Lo hize de 2 opciones ya que de la primera, se me muestran los 10 id de clock_speed pero la grafica se me muestran muy finos
+    en la otra opcion, lo agrupamos por clock_speed pero solo se me muestran los 6 id de clock_speed pero me sale bien la grafica """
+    clock = data["clock_speed"]
+    #clock = data.groupby('clock_speed')['clock_speed'].agg(len)
+    return clock
+    print(clock)
+
+# 2n Funcion para mostrar los mega pixels por los id que le pasemos
+def mostrar_mega_pixels(id_a_filtrar):
+    data = leer_y_filtrar_datos(id_a_filtrar)
+    #px = data["px_width"]
+    px = data.groupby('px_width')['px_width'].agg(len)
+    return px
+    print(px)
+
+# 3r Funcion para mostrar los datos de battery_power por los id que le pasemos
+def mostrar_battery_power(id_a_filtrar):
+    data = leer_y_filtrar_datos(id_a_filtrar)
+    battery = data["battery_power"]
+    #battery = data.groupby('battery_power')['battery_power'].agg(len)
+    return battery
+    print(battery)
--- main.py
+++ main.py
@@ -1,14 +1,54 @@
 import matplotlib.pyplot as plt
-import pandas as pd
-
 import ejercicio3 as ejercicio3
 
-ejercicio3.velocidadMicro()
+## Definir los id a filtrar que en todo caso serian las 10 muestras a mostrar.
+id_a_filtrar = [3,13,34,56,70,85,110,120,210,400]
+### Llamar a todas las funciones definida que esta en el archivo ejercicio3.py
+leer_datos_data = ejercicio3.leer_y_filtrar_datos(id_a_filtrar)
+clock_speed_data = ejercicio3.mostrar_clock_speed(id_a_filtrar)
+megapixels_data = ejercicio3.mostrar_mega_pixels(id_a_filtrar)
+battery_power_data = ejercicio3.mostrar_battery_power(id_a_filtrar)
 
-df = pd.read_csv("test.csv")
-x = df["id"]
-y = df["clock_speed"]
-colors = 'blue'
-plt.xlabel('clock_speed')
-plt.bar(x,y, color=colors)
-plt.show()
+## Definir los colores de la grafica Generica
+colores = ['red','blue','green','orange','pink','lightblue','yellow']
+## Definir el tamaño de mi grafica de 8x5 Generica
+fig, ax = plt.subplots(figsize= (8,5))
+
+# 1r Funcion para mostrar la grafica 1 de Clock_speed
+def grafica_barras1(data, nombre_eje_x):
+    plt.xlabel("Clock Speed")
+    plt.ylabel("Count")
+    plt.bar(data, nombre_eje_x, color=colores)
+    plt.show()
+
+# 2n Funcion para mostrar la grafica 2 de Mega Pixels
+def grafica_barras2(data, nombre_eje_y):
+    plt.xlabel("Mega Pixels")
+    plt.ylabel("Count")
+    plt.bar(data, nombre_eje_y, color=colores)
+    plt.show()
+
+# 3n Funcion para mostrar la grafica 3 de Battery Power
+def grafica_barras3(data, nombre_eje_z):
+    plt.xlabel("Battery Power")
+    plt.ylabel("Count")
+    plt.bar(data, nombre_eje_z, color=colores)
+    plt.show()
+
+# Funcion Principal para el menu de opciones y para llamar a mis 3 funciones
+def inicio():
+    entero = int(input("Que funcion quieres llamar?\n1)Mostrar el grafico de Clock_speed\n2)Mostrar el grafico de Mega Pixels\n3)Mostrar el grafico de Battery_power\nNumero a introducir:"))
+
+    if(entero == 1):
+        grafica_barras1(clock_speed_data.index,clock_speed_data.values)
+        print(clock_speed_data)
+    elif(entero == 2):
+       grafica_barras2(megapixels_data.index, megapixels_data.values)
+       print(megapixels_data)
+    elif(entero == 3):
+        grafica_barras3(battery_power_data.index, battery_power_data.values)
+        print(battery_power_data)
+    else:
+        print("Introduce una de las 3 opcions (1-2-3)\n")
+        inicio()
+inicio()
