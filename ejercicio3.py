6339fe4de42457d18ff75cba4c69d51f31e9d503
JonathanValleA <jvalle@jaumebalmes.net>
2023-01-17 19:54:10 +0100

    Implementar grafica en las 3 funciones creadas en el ejercicio3.py

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,8 +1,29 @@
+# Importamos pandas
 import pandas as pd
-import matplotlib.pyplot as plt
-def velocidadMicro():
-    df = pd.read_csv("test.csv", usecols=["clock_speed"])
-    print(df)
 
-def megapixels():
-    df = pd.read_csv("")
+### Funcion Principal para la lectura de archivo csv y filtrar los datos por el id ###
+def leer_y_filtrar_datos(id_a_filtrar):
+    df = pd.read_csv("test.csv")
+    id = df[df['id'].isin(id_a_filtrar)]
+    return id
+
+# 1r Funcion para mostrar los datos de clock_speed por los id que le pasemos
+def mostrar_clock_speed(id_a_filtrar):
+    data = leer_y_filtrar_datos(id_a_filtrar)
+    clock = data.groupby('clock_speed')['clock_speed'].agg(len)
+    return clock
+    print(clock)
+
+# 2n Funcion para mostrar los mega pixels por los id que le pasemos
+def mostrar_mega_pixels(id_a_filtrar):
+    data = leer_y_filtrar_datos(id_a_filtrar)
+    px = data.groupby('px_width')['px_width'].agg(len)
+    return px
+    print(px)
+
+# 3r Funcion para mostrar los datos de battery_power por los id que le pasemos
+def mostrar_battery_power(id_a_filtrar):
+    data = leer_y_filtrar_datos(id_a_filtrar)
+    battery = data.groupby('battery_power')['battery_power'].agg(len)
+    return battery
+    print(battery)
--- main.py
+++ main.py
@@ -1,14 +1,46 @@
 import matplotlib.pyplot as plt
-import pandas as pd
-
 import ejercicio3 as ejercicio3
 
-ejercicio3.velocidadMicro()
+id_a_filtrar = [3,13,34,56,70,85,110,120,210,400]
+leer_datos_data = ejercicio3.leer_y_filtrar_datos(id_a_filtrar)
+clock_speed_data = ejercicio3.mostrar_clock_speed(id_a_filtrar)
+megapixels_data = ejercicio3.mostrar_mega_pixels(id_a_filtrar)
+battery_power_data = ejercicio3.mostrar_battery_power(id_a_filtrar)
 
-df = pd.read_csv("test.csv")
-x = df["id"]
-y = df["clock_speed"]
-colors = 'blue'
-plt.xlabel('clock_speed')
-plt.bar(x,y, color=colors)
-plt.show()
+colores = ['red','blue','green','orange','pink','lightblue','yellow']
+def grafica_barras1(data, nombre_eje_x):
+    fig, ax = plt.subplots(figsize= (8,5))
+    plt.xlabel("Clock Speed")
+    plt.ylabel("Count")
+    plt.bar(data, nombre_eje_x, color=colores)
+    plt.show()
+def grafica_barras2(data, nombre_eje_y):
+    fig, ax = plt.subplots(figsize=(8, 5))
+    plt.xlabel("Mega Pixels")
+    plt.ylabel("Count")
+    plt.bar(data, nombre_eje_y, color=colores)
+    plt.show()
+
+def grafica_barras3(data, nombre_eje_z):
+    fig, ax = plt.subplots(figsize=(8, 5))
+    plt.xlabel("Battery Power")
+    plt.ylabel("Count")
+    plt.bar(data, nombre_eje_z, color=colores)
+    plt.show()
+
+def inicio():
+    entero = int(input("Que funcion quieres llamar?\n1)Mostrar el grafico de Clock_speed\n2)Mostrar el grafico de Mega Pixels\n3)Mostrar el grafico de Battery_power\nNumero a introducir:"))
+
+    if(entero == 1):
+        grafica_barras1(clock_speed_data.index,clock_speed_data.values)
+        print(clock_speed_data)
+    elif(entero == 2):
+       grafica_barras2(megapixels_data.index, megapixels_data.values)
+       print(megapixels_data)
+    elif(entero == 3):
+        grafica_barras3(battery_power_data.index, battery_power_data.values)
+        print(battery_power_data)
+    else:
+        print("Introduce una de las 3 opcions (1-2-3)\n")
+        inicio()
+inicio()
