0293d416a8b00f6f5ecd3212da29bfb6b777227a
JonathanValleA <113449282+JonathanValleA@users.noreply.github.com>
2023-01-19 21:38:15 +0100

    Branca jonathan (#3)
    * Implementar grafica en las 3 funciones creadas en el ejercicio3.py
    
    * Agregar comentarios
    
    * Graficas arregladas, se mostraban muy finas

--- ejercicio3.py
+++ ejercicio3.py
@@ -3,32 +3,34 @@ import pandas as pd
 
 ### Funcion Principal para la lectura de archivo csv y filtrar los datos por el id ###
 def leer_y_filtrar_datos(id_a_filtrar):
+    # Leemos el archivo csv con el nombre de test.csv
     df = pd.read_csv("test.csv")
+    # Hacemos el filtro por id y le pasamos un arreglo de todas las id a mostrar
     id = df[df['id'].isin(id_a_filtrar)]
-    return id
+    return id # Retornamos el id o sino no nos devolvera nada
 
 # 1r Funcion para mostrar los datos de clock_speed por los id que le pasemos
 def mostrar_clock_speed(id_a_filtrar):
+    # Llamamos a la funcion donde se muestra los id pasados
     data = leer_y_filtrar_datos(id_a_filtrar)
-    """ Lo hize de 2 opciones ya que de la primera, se me muestran los 10 id de clock_speed pero la grafica se me muestran muy finos
-    en la otra opcion, lo agrupamos por clock_speed pero solo se me muestran los 6 id de clock_speed pero me sale bien la grafica """
+    # De esas id/muestras, mostramos solamente la columna de clock_speed
     clock = data["clock_speed"]
-    #clock = data.groupby('clock_speed')['clock_speed'].agg(len)
+    # Retornamos Clock
     return clock
     print(clock)
 
 # 2n Funcion para mostrar los mega pixels por los id que le pasemos
 def mostrar_mega_pixels(id_a_filtrar):
     data = leer_y_filtrar_datos(id_a_filtrar)
-    #px = data["px_width"]
-    px = data.groupby('px_width')['px_width'].agg(len)
+    # Aqui buscamos el px_width
+    px = data["px_width"]
     return px
     print(px)
 
 # 3r Funcion para mostrar los datos de battery_power por los id que le pasemos
 def mostrar_battery_power(id_a_filtrar):
     data = leer_y_filtrar_datos(id_a_filtrar)
+    # Aqui buscamos la columna Battery power
     battery = data["battery_power"]
-    #battery = data.groupby('battery_power')['battery_power'].agg(len)
     return battery
     print(battery)
